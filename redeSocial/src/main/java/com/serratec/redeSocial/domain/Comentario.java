508a4e876876dbfcae7babd77b9c0f5714836bfe
CaiqueROC <caique_ramos2010@hotmail.com>
2024-10-16 15:11:22 -0300

    complementação classe comentario

--- redeSocial/src/main/java/com/serratec/redeSocial/domain/Comentario.java
+++ redeSocial/src/main/java/com/serratec/redeSocial/domain/Comentario.java
@@ -1,5 +1,63 @@
 package com.serratec.redeSocial.domain;
 
+import java.time.LocalDate;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Comentario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String texto;
+
+	private LocalDate dataCriacao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDate getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(LocalDate dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Comentario other = (Comentario) obj;
+		return Objects.equals(id, other.id);
+	}
 
 }
