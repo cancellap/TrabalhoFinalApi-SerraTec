bbf2df537dcc7a4cab129117ff7fcfdfb16f4939
MtAguiar <matheus.saguiar@hotmail.com>
2024-10-16 14:59:13 -0300

    Adicionando algumas novas classes

---
+++ redeSocial/src/main/java/com/serratec/redeSocial/controller/PostagemController.java
@@ -0,0 +1,4 @@
+package com.serratec.redeSocial.controller;
+
+public class PostagemController {
+}
---
+++ redeSocial/src/main/java/com/serratec/redeSocial/repository/PostagemRepository.java
@@ -0,0 +1,4 @@
+package com.serratec.redeSocial.repository;
+
+public interface PostagemRepository {
+}
