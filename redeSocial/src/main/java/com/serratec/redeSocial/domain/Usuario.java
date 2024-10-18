c36019954f9389d6947bfd3bf90165a9db533db5
MtAguiar <matheus.saguiar@hotmail.com>
2024-10-18 17:24:34 -0300

    Atualizando meu repositório local

--- redeSocial/src/main/java/com/serratec/redeSocial/domain/Usuario.java
+++ redeSocial/src/main/java/com/serratec/redeSocial/domain/Usuario.java
@@ -2,125 +2,107 @@ package com.serratec.redeSocial.domain;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Objects;
-
-import jakarta.persistence.*;
+import java.util.Set;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Usuario {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@Column(name="nome_usuario")
-	private String nome;
+    @Column(name = "nome_usuario")
+    private String nome;
 
-	@Column(name="sobrenome_usuario")
-	private String sobrenome;
+    @Column(name = "sobrenome_usuario")
+    private String sobrenome;
 
-	@Column(name="email_usuario")
-	private String email;
+    @Column(name = "email_usuario")
+    private String email;
 
-	@Column(name="senha_usuario")
-	private String senha;
+    @Column(name = "senha_usuario")
+    private String senha;
 
-	@Column
-	private LocalDate dataNascimento;
-	
-	//Set<Relacionamento> seguidores 
-	
-	//Set<Relacionamento> seguindo
+    @Column
+    private LocalDate dataNascimento;
 
-	//Relação um para muitos entre Usuário e postagem. Não sei se precisaremos das anotações JsonBackReference e IgnoreJson
-	@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
-	private List<Postagem> postagens;
+    @OneToMany(mappedBy = "seguidor", cascade = CascadeType.ALL)
+    private Set<Relacionamento> seguidor;
 
-	public Usuario(Long id, String nome, String sobrenome, String email, String senha, LocalDate dataNascimento) {
-		super();
-		this.id = id;
-		this.nome = nome;
-		this.sobrenome = sobrenome;
-		this.email = email;
-		this.senha = senha;
-		this.dataNascimento = dataNascimento;
-	}
+    @OneToMany(mappedBy = "seguido", cascade = CascadeType.ALL)
+    private Set<Relacionamento> seguido;
 
-	public Usuario() {
-		super();
-	}
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
+    private List<Postagem> postagens;
 
-	public Long getId() {
-		return id;
-	}
+    public Usuario(Long id, String nome, String sobrenome, String email, String senha, LocalDate dataNascimento) {
+        super();
+        this.id = id;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+        this.senha = senha;
+        this.dataNascimento = dataNascimento;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public Usuario() {
+        super();
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getSobrenome() {
-		return sobrenome;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public void setSobrenome(String sobrenome) {
-		this.sobrenome = sobrenome;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getSobrenome() {
+        return sobrenome;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
 
-	public String getSenha() {
-		return senha;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public LocalDate getDataNascimento() {
-		return dataNascimento;
-	}
+    public String getSenha() {
+        return senha;
+    }
 
-	public void setDataNascimento(LocalDate dataNascimento) {
-		this.dataNascimento = dataNascimento;
-	}
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 
-	public List<Postagem> getPostagens() {
-		return postagens;
-	}
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
 
-	public void setPostagens(List<Postagem> postagens) {
-		this.postagens = postagens;
-	}
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Usuario other = (Usuario) obj;
-		return Objects.equals(id, other.id);
-	}
-
-}
+        }
