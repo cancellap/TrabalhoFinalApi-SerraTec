99b6d11ba4778ededa8165931b791cb98d08b7f4
Gabriel Braga <bragagabriel843@gmail.com>
2024-10-19 00:08:16 -0300

    implementacao de autenticacao por token jwt

---
+++ redeSocial/src/main/java/com/serratec/redeSocial/security/UsuarioDetalheImpl.java
@@ -0,0 +1,25 @@
+package com.serratec.redeSocial.security;
+
+import com.serratec.redeSocial.domain.Usuario;
+import com.serratec.redeSocial.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioDetalheImpl implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByEmail(username);
+        if (usuario == null) {
+            throw new RuntimeException();
+        }
+        return usuario;
+    }
+}
